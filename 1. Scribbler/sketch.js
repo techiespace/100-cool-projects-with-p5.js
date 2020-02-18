8fedd40300c9fed4afa0c2f4f523a4622cee4f50
Shreyas Waghmare <shreyas.waghmare@zs.com>
2020-02-18 10:17:04 +0530

    Added first sketch

---
+++ 1. Scribbler/sketch.js
@@ -0,0 +1,16 @@
+//https://github.com/techiespace/100-cool-projects-with-p5.js
+function setup() {
+  createCanvas(600, 500);
+  background(255);
+}
+
+function draw() {
+  text("Click the screen to clear the scribble", 20, 15);
+  noStroke();
+  fill(128, 0, 128, 80);
+  ellipse(mouseX, mouseY, 10, 10);
+}
+
+function mousePressed() {
+  background(255);
+}
